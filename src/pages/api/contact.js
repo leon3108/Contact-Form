022c434e25e4a5f3a164523265ca22c5b4d4d22e
leon <maxime.noel@epitech.eu>
2023-01-19 09:29:31 +0100

    feat: send mail with text informations but not the files

---
+++ src/pages/api/contact.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const PASSWORD = process.env.password
+
+export default function (req, res) {
+  let nodemailer = require('nodemailer')
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    port: 587,
+    host: "smtp.gmail.com",
+    auth: {
+      user: 'contactguillaumemail@gmail.com',
+      pass: PASSWORD,
+    },
+    secure: false,
+  });
+
+  const mailData = {
+    from: 'contactguillaumemail@gmail.com', //l'adresse qui expédiera le mail
+    to: 'maxnoelsens@gmail.com', // à mon adresse pour le moment puis celle de guillaume
+    subject: `Message From ${req.body.subject}`,
+    text: `${req.body.firstName} ${req.body.lastName}, ${req.body.email} \n\n${req.body.subject} \n\n${req.body.message}\n\n${req.body.files}`,
+    // html: <div>{req.body.files}</div>
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err)
+      console.log("err = " + err)
+    else
+      console.log("info = " + info)
+  })
+
+  console.log("req.body = " + req.body)
+  res.status(200)
+  res.end()
+}
